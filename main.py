c689f7605beed504e733c431fa817f7feafc94e3
imzeki <54060657+imzeki@users.noreply.github.com>
2019-08-17 22:32:50 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+Song = "Old Town Road(Remix)" #The name of the song
+Genre = "Country Song" #Genre of the song 
+Year = 2019 #Year it was realeased
+"""
+Create a shortcut to define the rest of the variables
+They are a group of variables to show the details of the song
+"""  
+Featured, Artist, DurationInSeconds, Director, Producer, EP, DP, ProductionDesigner, ArtDirector, LeadHorseWrangler, Costumer, Editor, OriginalScore, SoundDesign, DurationInMinutesAndSeconds  = "Billy Ray Cyrus", "Lil Nas X", 308, "Calmatic", "Candice Dragonas, Saul Levitz, Melissa Larsen Ekholm", "Bryan Younce", "Ryan Marie Helfant", "Christian Zollenkopf", "Itaru Dela Vega", "Scott Perez", "Catherine Hahn", "Calmatic", "Ian Hultquist", "Matt Yocum", "3.08" 
+print("Song's Name", Song) 
+print("Type of Music : ", Genre) 
+print("Year Realeased : ", Year)  
+print("Featured : ", Featured) 
+print("Artist : ", Artist) 
+print("Duration In Seconds : ", DurationInSeconds) 
+print("Director : ", Director) 
+print("Producer : ", Producer) 
+print("EP : ", EP) 
+print("DP : ", DP) 
+print("Production Designer : ", ProductionDesigner) 
+print("Art Director : ", ArtDirector), 
+print("Lead Horse Wrangler : ", LeadHorseWrangler) 
+print("Costumer : ", Costumer)  
+print("Editor : ", Editor) 
+print("Original Score : ", OriginalScore) 
+print("Sound Designer : ", SoundDesign)
+print("Duration In Minutes and Seconds : ", DurationInMinutesAndSeconds)
+
